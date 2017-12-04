d471a5f1741b431d510ff2c8e8915e16050ceae7
seifhussamSe <seifel-din.mostafa@student.guc.edu.eg>
2017-12-04 17:04:14 +0200

    summary enhancements

--- searchFunc.go
+++ searchFunc.go
@@ -25,8 +25,8 @@ func FindTalk(min string) string {
 			if strings.Contains(strings.ToLower(e1.talkName), strings.ToLower(min)) || strings.Contains(strings.ToLower(min), strings.ToLower(e1.talkName)) {
 				res = res + strings.Title(e.firstName) + " " + strings.Title(e.lastName) + " : " + e1.talkName
 
-				res = res + " \nTalk Summary : " + fetchSummary(e1.talkKeyWord, e1.nameKeyword, e1.speakerKeyword) /* fetchAltSummary(e1.nameKeyword)*/ + "<br>"
-				res = res + " \nVideo Link : " + fetchVideoLink(e1.talkName)
+				res = res + " \nTalk Summary : " + fetchSummary(e1.talkKeyWord, e1.nameKeyword, e1.speakerKeyword) /* fetchAltSummary(e1.nameKeyword)*/
+				res = res + " \nVideo Link : " + fetchVideoLink(e1.talkName) + "<br>"
 				x++
 				break
 			}
@@ -49,8 +49,8 @@ func searchSpeakername(min string) string {
 			for _, e1 := range e.talks {
 				res = res + strings.Title(e.firstName) + " " + strings.Title(e.lastName) + " : " + e1.talkName
 
-				res = res + " \nTalk Summary : " + fetchSummary(e1.talkKeyWord, e1.nameKeyword, e1.speakerKeyword) /* fetchAltSummary(e1.nameKeyword)*/ + "<br>"
-				res = res + " \nVideo Link : " + fetchVideoLink(e1.talkName)
+				res = res + " \nTalk Summary : " + fetchSummary(e1.talkKeyWord, e1.nameKeyword, e1.speakerKeyword) /* fetchAltSummary(e1.nameKeyword)*/
+				res = res + " \nVideo Link : " + fetchVideoLink(e1.talkName) + "<br>"
 				x++
 			}
 			if x > 5 {
